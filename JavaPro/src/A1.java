1d0c113e87c25c50f938461f4c26a72d253b6599
ponjagannath <ponjagannath@gmail.com>
2018-05-11 17:30:12 +0530

    did it

--- JavaPro/src/A1.java
+++ JavaPro/src/A1.java
@@ -4,7 +4,8 @@ public class A1 {
 	public static void main(String[] args) {
 		System.out.println("Eneter message here");
 		System.out.println("Learning");
-		
+		System.out.println("something");
+		System.out.println("abcd");
 	}
 
 }
